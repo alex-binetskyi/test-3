4529b9e1f1cbe7e4d2bf78990fd2332aaaa240e5
Alex Binetskyi <alex.binetskyi.binary@gmail.com>
2020-09-15 09:51:43 +0300

    Add all files finished TestTask3.
    Заливаю все исходики работы выполенного тестового задание.

---
+++ #src/js/script.js
@@ -0,0 +1,4 @@
+@@include('../../node_modules/jquery/dist/jquery.min.js');
+@@include('../../node_modules/picturefill/dist/picturefill.min.js');
+@@include('vendors/webp-check.js');
+@@include('vendors/smooth-scroll.js');
